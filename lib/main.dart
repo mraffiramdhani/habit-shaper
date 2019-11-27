863e31fc7c75e47ac6fb947de11b2f0550100506
mraffiramdhani <raffi.programming@gmail.com>
2019-11-27 17:27:04 +0700

    Add Splash Screen and Some Patch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:habit_shaper/homeScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +9,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Habit Shaper',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Habit Shaper'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Habit Shaper',
+      home: new SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        '/HomeScreen': (BuildContext context) => new HomeScreen()
+      },
     );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  return new Scaffold(
+    body: new Center(
+      child: new Image.asset('assets/images/placeholder.png', width: 200.0,),
+    ),
+  );
   }
 }
