7c10826b9c4950fd99ada047c1a6ca8f52977b10
mraffiramdhani <raffi.programming@gmail.com>
2019-11-28 17:45:04 +0700

    Minor Update

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Habit Shaper',
       home: new SplashScreen(),
       routes: <String, WidgetBuilder>{
@@ -25,7 +26,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   startTime() async {
-    var _duration = new Duration(seconds: 2);
+    var _duration = new Duration(seconds: 3);
     return new Timer(_duration, navigationPage);
   }
 
@@ -41,10 +42,62 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-  return new Scaffold(
-    body: new Center(
-      child: new Image.asset('assets/images/placeholder.png', width: 200.0,),
-    ),
-  );
+    return new Scaffold(
+      body: Stack(
+        children: <Widget>[
+          WavyHeader(),
+          Center(
+            child: Text("Habit Shaper", style: TextStyle(fontSize: 50.0, fontFamily: "Satisfy", color: Colors.black87))
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class WavyHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          child: Container(width: double.infinity, height: 150.0, color: Color.fromARGB(255, 237, 237, 237),),
+          clipper: BottomWaveClipper(),
+        ),
+        ClipPath(
+          child: Container(width: double.infinity, height: 125.0, color: Color.fromARGB(255, 217, 217, 217),),
+          clipper: BottomWaveClipper(),
+        )
+      ],
+    );
+  }
+}
+
+class BottomWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size){
+    var path = Path();
+    path.lineTo(0.0, size.height - 20);
+
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2.25, size.height - 30.0);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    var secondControlPoint =
+        Offset(size.width - (size.width / 3.25), size.height - 65);
+    var secondEndPoint = Offset(size.width, size.height - 40);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, size.height - 40);
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+
 }
